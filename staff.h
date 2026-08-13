3b5a0a7bbab1b57449e721ce649765b5a5b19a96
maksuda77462 <maksudaakter77462@gmail.com>
2026-08-13 11:00:30 +0600

    Refactor staff.h by removing struct and modifying functions
    Removed the Staff struct and updated function declarations.

--- staff.h
+++ staff.h
@@ -1,17 +1,8 @@
-#ifndef STAFF_H
+ #ifndef STAFF_H
 #define STAFF_H
 
-struct Staff {
-    int id;
-    char name[50];
-    char position[50];
-    float salary;
-    char phone[15];
-};
-
 void addStaff();
-void viewAllStaff();
+void viewStaff();
 void searchStaff();
-void staffMenu();
 
 #endif
